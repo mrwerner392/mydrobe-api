25445c777407b50ccd73956ba2294e26ce95387a
mrwerner392 <mrwerner392@gmail.com>
2021-05-28 22:33:49 -0400

    User create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
 
+  def create
+    username = params[:username]
+    password = params[:password]
+    password_confirmation = params[:password_confirmation]
+
+    if password != password_confirmation
+      render json: { message: 'Passwords do not match.' }, status: :unprocessable_entity and return
+    end
+
+    user = User.create(username: username, password: password)
+    if user.valid?
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
 end
