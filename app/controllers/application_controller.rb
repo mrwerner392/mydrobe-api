ac6cb44e017721686990b0bc533b3ef9ef0a657f
mrwerner392 <mrwerner392@gmail.com>
2021-05-29 14:38:18 -0400

    Create jwt token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  def authentication_json(user_id)
+    { token: make_token(user_id), user: User.find(user_id) }
+  end
+
+  def make_token(user_id)
+    payload = {user_id: user_id}
+    JWT.encode(payload, hmac_secret, 'HS256')
+  end
+
+  def hmac_secret
+    ENV["JWT_SECRET_KEY"]
+  end
+
 end
