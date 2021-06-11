59fc3cb20ba8a8fd3ff1388f63450e6fdb3a7b0b
mrwerner392 <mrwerner392@gmail.com>
2021-06-10 21:58:04 -0400

    Serve token with user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     user = User.create(username: username, password: password)
     if user.valid?
-      render json: user
+      render json: application_json(user)
     else
       render json: { message: user.errors.full_messages.first }, status: :unprocessable_entity
     end
